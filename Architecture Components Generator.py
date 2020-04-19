cbfd3c9e2dc412de74a1ce2d88f812e6c1ff0f18
Arina <43216874+rereshek@users.noreply.github.com>
2020-04-19 01:05:10 -0400

    edited GUI

--- Architecture Components Generator.py
+++ Architecture Components Generator.py
@@ -15,7 +15,12 @@ cmds.menuItem(label="Delete Selected", command=('cmds.delete()'))
 
 #            UI: adjust and create Straight Stairs              #
 #################################################################
-cmds.frameLayout(collapsable=True, label="Straight Stairs", width=475, height=40)
+cmds.frameLayout(collapsable=True, label="Straight Stairs", width=475)
+
+cmds.columnLayout()
+cmds.radioButtonGrp('straightStairsHeight', label="Staircase Height", labelArray3=["2.4 m", "2.8 m", "3.2 m"], numberOfRadioButtons=3, sl=2)
+cmds.intSliderGrp('straightStairsWidth', l="Staircase Width", f=True, min=1, max=6, value=3)
+cmds.radioButtonGrp('straightStairsHR', label="Handrails", labelArray2=["yes", "no"], numberOfRadioButtons=2, sl=1)
 
 cmds.columnLayout()
 cmds.button(label="Create Straight Stairs", command=('straightStairs()'))
@@ -33,6 +38,8 @@ cmds.columnLayout()
 cmds.radioButtonGrp('spiralStairsHeight', label="Staircase Height", labelArray3=["2.4 m", "2.8 m", "3.2 m"], numberOfRadioButtons=3, sl=2)
 cmds.radioButtonGrp('spiralStaircaseDiameter', label="Staircase Diameter", labelArray3=["140 m", "160 m", "180 m"], numberOfRadioButtons=3, sl=1)
 cmds.intSliderGrp('stepHeight', l="Step Thickness", f=True, min=1, max=6, value=3)
+cmds.colorSliderGrp('baseColour', label="Colour", hsv=(120, 1, 1))
+cmds.colorSliderGrp('stairsColour', label="Colour", hsv=(120, 1, 1))
 
 cmds.columnLayout()
 cmds.button(label="Create Spiral Stairs", command=('spiralStairs()'))
@@ -107,6 +114,12 @@ cmds.setParent( '..' )
 cmds.frameLayout(collapsable=True, label="Walls with window/door frames")
 
 cmds.columnLayout()
+cmds.radioButtonGrp('wallHeight', label="Wall Height", labelArray3=["2.4 m", "2.8 m", "3.2 m"], numberOfRadioButtons=3, sl=2)
+cmds.radioButtonGrp('windowHeight', label="Wall Height", labelArray3=["1 m", "1.5 m", "2 m"], numberOfRadioButtons=3, sl=2)
+cmds.radioButtonGrp('addDoor', label="Door", labelArray2=["yes", "no"], numberOfRadioButtons=2, sl=1)
+cmds.intSliderGrp('windows', l="Number of Windows", f=True, min=1, max=3, value=3)
+
+cmds.columnLayout()
 cmds.button(label="Create Walls with window/door frames", command=('walls()'))
 
 # Level Up in Hierarchy
@@ -120,6 +133,9 @@ cmds.showWindow( myWin )
 #################################################################
 def straightStairs():
     # query user input values
+    staircaseHeight = cmds.radioButtonGrp('straightStairsHeight', q=True, sl=True)
+    staircaseWidth = cmds.intSliderGrp('straightStairsWidth', q=True, v=True)
+    createHandrails = cmds.radioButtonGrp('straightStairsHR', q=True, sl=True)
     
     # define stairs size
     stairSizeX = 8.0
@@ -134,7 +150,7 @@ def straightStairs():
     cmds.namespace(set=nsTmp)
     
     # step count
-    for i in range (8):
+    for i in range (16):
         # create a stair
         cmds.polyCube(d = stairSizeZ, h = stairSizeY, w = stairSizeX)
         # move it on Z axis
@@ -147,46 +163,49 @@ def straightStairs():
     # move them up
     cmds.move(stairSizeY/2.0, moveY=True)    
        
+    # create handrails
+    if(createHandrails == 1): 
+   
+        # handrail poles Left
+        for i in range (8):
+            # create Left side poles
+            cmds.polyCylinder (r = 0.05, h = 6)
+            # move it up
+            cmds.move(3 + i*stairSizeY, moveY=True)
+            # move it to the left
+            cmds.move(-stairSizeX/2.0 + 0.4, moveX=True)
+            # move it into depth
+            cmds.move(-i *stairSizeZ, moveZ=True)
+            
+        # handrail poles Right
+        for i in range (8):
+            # create Left side poles
+            cmds.polyCylinder (r = 0.05, h = 6)
+            # move it up
+            cmds.move(3 + i*stairSizeY, moveY=True)
+            # move it to the Right
+            cmds.move(stairSizeX/2.0 - 0.4, moveX=True)
+            # move it into depth
+            cmds.move(-i *stairSizeZ, moveZ=True)
         
-    # handrail poles Left
-    for i in range (8):
-        # create Left side poles
-        cmds.polyCylinder (r = 0.05, h = 6)
-        # move it up
-        cmds.move(3 + i*stairSizeY, moveY=True)
-        # move it to the left
-        cmds.move(-stairSizeX/2.0 + 0.4, moveX=True)
-        # move it into depth
-        cmds.move(-i *stairSizeZ, moveZ=True)
+            
+        # handrail upper part 
+        # length^2 = a^2 + b^2
         
-    # handrail poles Right
-    for i in range (8):
-        # create Left side poles
-        cmds.polyCylinder (r = 0.05, h = 6)
-        # move it up
-        cmds.move(3 + i*stairSizeY, moveY=True)
-        # move it to the Right
-        cmds.move(stairSizeX/2.0 - 0.4, moveX=True)
-        # move it into depth
-        cmds.move(-i *stairSizeZ, moveZ=True)
-    
         
-    # handrail upper part 
-    # length^2 = a^2 + b^2
-    
-    #LEFT
-    cmds.polyCube( sx=1, sy=1, sz=1, h=.2, w=.5, d=17)
-    
-    cmds.rotate(27, 0, 0)
-    
-    cmds.move(-3.557, 9.576, -6.858)
-    
-    # RIGHT
-    cmds.polyCube( sx=1, sy=1, sz=1, h=.2, w=.5, d=17)
-    
-    cmds.rotate(27, 0, 0)
-    
-    cmds.move(3.557, 9.576, -6.858)
+        #LEFT
+        cmds.polyCube( sx=1, sy=1, sz=1, h=.2, w=.5, d=17)
+        
+        cmds.rotate(27, 0, 0)
+        
+        cmds.move(-3.557, 9.576, -6.858)
+        
+        # RIGHT
+        cmds.polyCube( sx=1, sy=1, sz=1, h=.2, w=.5, d=17)
+        
+        cmds.rotate(27, 0, 0)
+        
+        cmds.move(3.557, 9.576, -6.858)
     
 #################################################################
 #                     SPIRAL STAIRS FUNCTION                    #  
@@ -196,6 +215,8 @@ def spiralStairs():
     staircaseHeight = cmds.radioButtonGrp('spiralStairsHeight', q=True, sl=True)
     staircaseDiameter = cmds.radioButtonGrp('spiralStaircaseDiameter', q=True, sl=True)
     stepThickness = cmds.intSliderGrp('stepHeight', q=True, v=True)
+    rgbB = cmds.colorSliderGrp('baseColour', q=True, rgbValue=True)
+    rgbS = cmds.colorSliderGrp('stairsColour', q=True, rgbValue=True) 
      
     # define variables   
     stepHeight = stepThickness * 0.3
@@ -217,20 +238,23 @@ def spiralStairs():
     if(staircaseDiameter == 3):
         stairWidth = 9.4    # 180m
     
-    # base
+    # create base
     cmds.polyCylinder(r = 2, h = cylindHeight)
+    
     # move it up
     cmds.move(cylindHeight/2, moveY=True)
+    
+    # add colour      
+    
+
     # step count 
     for i in range(stepCount):
         # stair
         cmds.polyCube(w = stairWidth, d = 2, h = stepHeight)
-        # move it half size up from center
-        cmds.move(stepHeight/2, moveY=True)
         # move it on X axis 
         cmds.move(3, moveX=True)
         # move it on Y axis 
-        cmds.move(i*2, moveY=True)
+        cmds.move(i*2 + stepHeight/2, moveY=True)
         # move rotation pivot and set world-space transformation
         cmds.xform( ws = True, rotatePivot = (0, 0, 0))
         # rotate on Y axis
